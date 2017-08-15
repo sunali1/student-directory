baab79e83f431b66d98e72d15b126b47e2983e16
sunaliseth <sunaliseth01@gmail.com>
2017-08-15 15:18:05 +0100

    Refactored line of student count

--- directory.rb
+++ directory.rb
@@ -13,7 +13,4 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 # finally we print the total number of students
-print "Overall, we have "
-#it's important that print() doesn't add new line characters
-print 11
-puts " great students"
+print "Overall, we have #{student_count} great students"
