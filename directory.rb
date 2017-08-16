a3a1cad48a7061e8aa2441942f406b8253127063
sunaliseth <sunaliseth01@gmail.com>
2017-08-16 22:35:29 +0100

    Step 8.9 changed students to student

--- directory.rb
+++ directory.rb
@@ -7,16 +7,20 @@ def input_students
   while !name.empty? do # note if ! is missing the input never ends
   puts "cohort?"
   cohort = gets.chomp.to_sym
-  cohort = 'November'.to_sym if cohort.empty?
+  cohort = "November".to_sym if cohort.empty?#default :november cohort didnt work
   puts "country of birth?"
   country_of_birth = gets.chomp
   puts "age?"
   age = gets.chomp
-    students << {name: name, cohort: :november, country_of_birth: country_of_birth, age: age}
-    puts "Now we have #{students.count} students. Next name?"
+    students << {name: name, cohort: cohort, country_of_birth: country_of_birth, age: age}
+    if students.count == 1
+    puts "Now we have #{students.count} student. Next name?"
+    else
+      puts "Now we have #{students.count} students. Next name?"
+    end
   name = gets.chomp
   end
-  students
+  students    # step 8.8 TRY LATER unable to do .group_by{|x| x[:cohort]} cant figure why
 end
 
 def print_header
@@ -26,7 +30,7 @@ end
 
 def print(students)
   i = 0
-  while i < students.size do
+  while i < students.size && students.size > 0 do
   ni = i + 1
   puts "#{ni}. #{students[i][:name].ljust(30)} (#{students[i][:cohort]} cohort) #{students[i][:country_of_birth]} #{students[i][:age]}" if students[i][:name].start_with?("T","t")
   i += 1
