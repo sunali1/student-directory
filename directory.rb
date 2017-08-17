5a4749496ab99c778802982ac697e08a7a5f3395
sunaliseth <sunaliseth01@gmail.com>
2017-08-17 15:09:38 +0100

    Added load list from csv file and added gitignore

--- directory.rb
+++ directory.rb
@@ -10,6 +10,7 @@ end
     puts "1. Input the students"
     puts "2. Show the students"
     puts "3. Save the list to stdents.csv"
+    puts "4. Load the list from students.csv"
     puts "9. Exit"
   end
 
@@ -27,6 +28,8 @@ end
     show_students
     when "3"
     save_students
+    when "4"
+    load_students
     when "9"
     exit # this will cause the program to terminate
     else
@@ -67,6 +70,15 @@ def save_students
     student_data = [student[:name]], [student[:cohort]]
     csv_line = student_data.join(" ")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+   name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: "cohort".to_sym}
   end
   file.close
 end
